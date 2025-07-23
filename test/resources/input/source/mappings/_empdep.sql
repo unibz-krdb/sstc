00d4065f6976f6971bd065685e64f26523c52dda
Aidan Bailey <dev@aidanbailey.me>
2025-07-23 09:42:27 +0200

    Add TODO

--- test/resources/input/source/mappings/_empdep.sql
+++ test/resources/input/source/mappings/_empdep.sql
@@ -1,4 +1,6 @@
+-- TODO: Maybe all select distinct
 {{ select_preamble }} {{ attributes }}
 FROM transducer._EMPDEP{{ primary_suffix }}
 NATURAL LEFT OUTER JOIN transducer._POSITION{{ secondary_suffix }}
 {{ where | default('WHERE ssn IS NOT NULL AND dep_address IS NOT NULL') }}
+-- TODO: Programatically generate the where is not null with primary keys from all join tables
