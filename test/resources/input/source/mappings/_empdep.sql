26282cb57cabf8e555c434af23e6765125a6dccb
Aidan Bailey <dev@aidanbailey.me>
2025-07-23 15:31:18 +0200

    Cut comment

--- test/resources/input/source/mappings/_empdep.sql
+++ test/resources/input/source/mappings/_empdep.sql
@@ -1,5 +1,4 @@
-SELECT DISTINCT {{ attributes }}
+SELECT DISTINCT {{ attributes | default('ssn, name, phone, email, dep_name, dep_address, city, country') }}
 FROM transducer._EMPDEP{{ primary_suffix }}
 NATURAL LEFT OUTER JOIN transducer._POSITION{{ secondary_suffix }}
 {{ where | default('WHERE ssn IS NOT NULL AND dep_address IS NOT NULL') }}
--- TODO: Programatically generate the where is not null with primary keys from all join tables
