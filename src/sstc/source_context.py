6fc230a9dce0ebd0fa0f0f72efe917faae9a3d0f
Aidan Bailey <dev@aidanbailey.me>
2025-10-07 16:05:39 +0200

    Add SourceContext

---
+++ src/sstc/source_context.py
@@ -0,0 +1,35 @@
+from rapt2.rapt import Rapt
+from rapt2.treebrd.node import DefinitionNode, DependencyNode, Node
+
+class SourceContext:
+
+    relations: list[DefinitionNode]
+    dependencies: dict[str, DependencyNode]
+
+    def __init__(self, relations: list[DefinitionNode], dependencies: dict[str, DependencyNode]):
+        self.relations = relations
+        self.dependencies = dependencies
+
+    @classmethod
+    def from_file(cls, file_path: str):
+        with open(file_path, 'r') as file:
+            content = file.read()
+        return cls.from_string(instring=content)
+
+    @classmethod
+    def from_string(cls, instring: str):
+        syntax_tree = Rapt(grammar="DependencyGrammar").to_syntax_tree(instring)
+        return cls.from_syntax_tree(syntax_tree=syntax_tree)
+
+    @classmethod
+    def from_syntax_tree(cls, syntax_tree: list[Node]):
+        relations = []
+        dependencies = {}
+        for node in syntax_tree:
+            if isinstance(node, DefinitionNode):
+                relations.append(node)
+            elif isinstance(node, DependencyNode):
+                dependencies[node.name] = node
+            else:
+                raise ValueError(f"Unexpected node type: {type(node)}")
+        return cls(relations=relations, dependencies=dependencies)
