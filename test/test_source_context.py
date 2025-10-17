8ceab9cf6206e6b8c282c487384eee713cd14e7d
Aidan Bailey <dev@aidanbailey.me>
2025-10-17 10:24:38 +0200

    Fix tests

--- test/test_source_context.py
+++ test/test_source_context.py
@@ -9,7 +9,6 @@ def test_source_context_from_file(example_1_dir: str):
     context = SourceContext.from_file(os.path.join(example_1_dir, "source.txt"))
     assert isinstance(context, SourceContext)
     assert len(context.source_tables) == 1
-    assert len(context.dependencies) == 7
     assert context.schema.to_dict() == {
         "person_ura": [
             "ssn",
@@ -28,4 +27,3 @@ def test_source_context_from_file(example_1_dir: str):
     )
     assert isinstance(context, TargetContext)
     assert len(context.relations) == 8
-    assert len(context.dependencies) == 16
