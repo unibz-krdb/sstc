70e01e50ca7ccfc18024e4f232d3d3eea5528fca
Aidan Bailey <dev@aidanbailey.me>
2025-11-05 13:36:42 +0200

    Add constructors

--- src/sstc/transducer.py
+++ src/sstc/transducer.py
@@ -1,7 +1,22 @@
+from .transducer_context import TransducerContext
+
+
 class Transducer:
 
-    def __init__(self):
-        pass
+    def __init__(self, ctx: TransducerContext):
+        self.ctx = ctx
+
+    @classmethod
+    def from_file(
+        cls, source_schema_path: str, source_constraints_path: str, target_path: str
+    ):
+        return cls(
+            TransducerContext.from_files(
+                source_schema_path=source_schema_path,
+                source_constraints_path=source_constraints_path,
+                target_path=target_path,
+            )
+        )
 
     def compile(self):
         raise NotImplementedError()
